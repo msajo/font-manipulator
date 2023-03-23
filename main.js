50dcb64cfb6f36eebb01a4f1d493e674c82fd536
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-03-23 16:07:05 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550,550);
+    canvas = createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded);
+
+    poseNet.on("pose" , gotposes);
+}
