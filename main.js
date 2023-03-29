8dbd60310b0fdc3db932433534102cdf5e7e9813
Sag is the best <87934102+msajo@users.noreply.github.com>
2023-03-29 16:51:05 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+noseX = 0;
+noseY = 0;
+left_wristX = 0;
+right_wristX = 0;
+difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(550,550);
@@ -13,5 +18,16 @@ function modelLoaded(){
 function gotposes(results){
     if(results.length > 0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        left_wristX = results[0].pose.leftWrist.x;
+        right_wristX = results[0].pose.rigthWrist.x;
+        difference = Math.floor(left_wristX - right_wristX);
     }
+}
+function draw(){
+    background("#ff26db");
+    textSize(difference);
+    fill("black");
+    text("texting",noseX,200);
 }
